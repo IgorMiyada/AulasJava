bd62c2d5fdd6ebc2e4fe29e3f11065d0034900e9
Igor miyada <igormiyada@gmail.com>
2025-05-14 11:07:05 -0300

    Construtores

---
+++ maratona-java/src/academy/devdojo/javacore/Dconstrutores/test/AnimeTest.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javacore.Dconstrutores.test;
+
+import academy.devdojo.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args){
+        Anime anime = new Anime("Akudama Drive","TV",12,"Ação");
+        anime.imprime();
+    }
+}
