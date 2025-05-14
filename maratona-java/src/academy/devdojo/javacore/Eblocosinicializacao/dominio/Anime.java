69706c806796fea1a1debc99878e816a1556d3fe
Igor miyada <igormiyada@gmail.com>
2025-05-14 12:29:33 -0300

    Blocos de inicialização

---
+++ maratona-java/src/academy/devdojo/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -0,0 +1,38 @@
+package academy.devdojo.javacore.Eblocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios = {1,2,3,4,5,6,7,8,9,10,11,12};
+    {
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+    public Anime(String nome){
+        this.nome = nome;
+    }
+    public Anime() {
+
+        for(int episodio:episodios){
+            System.out.print(episodio + " ");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(int[] episodios) {
+        this.episodios = episodios;
+    }
+}
---
+++ maratona-java/src/academy/devdojo/javacore/Eblocosinicializacao/test/AnimeTest.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javacore.Eblocosinicializacao.test;
+
+import academy.devdojo.javacore.Eblocosinicializacao.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+    }
+}
