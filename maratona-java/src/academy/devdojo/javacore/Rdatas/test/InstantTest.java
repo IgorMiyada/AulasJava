0170da9883eeafb72b59da593f233aafee1e00c7
Igor miyada <igormiyada@gmail.com>
2025-06-05 14:10:24 -0300

    Formatação de data

---
+++ maratona-java/src/academy/devdojo/javacore/Rdatas/test/InstantTest.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javacore.Rdatas.test;
+
+import java.time.Instant;
+
+public class InstantTest {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+    }
+}
